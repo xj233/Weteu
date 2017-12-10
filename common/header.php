a61cd68fb51d9a77c25d0f5385aa7470c7c7ff87
xj修行者 <vip115333347shequ@Hotmail.com>
2017-12-10 14:49:30 +0800

    优化header

---
+++ common/header.php
@@ -0,0 +1,25 @@
+<!Doctype html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<title>吐槽集能器</title>
+<meta name="description" content="留言板，本应简洁如此" />
+<meta name="keywords" content="风栖,矢量实验室,吐槽集能器,留言板,表白墙,萌到爆的叉鸡君" />
+<meta itemprop="name" content="吐槽集能器">
+<meta itemprop="image" content="https://www.feking.org/img/logo.png">
+<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no">
+<link rel="stylesheet" href="./common/style.css">
+<link rel="stylesheet" href="./common/OwO.min.css">
+<link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.css">
+<link rel="shortcut icon" href="./favicon.ico" /> 
+<hr>
+<center>
+	<ul class="nav nav-pills">
+	<li class="active"><a href="./index.php">Home</a></li>
+	<li><a href="./add.php">立即吐槽/上传图片</a></li>
+</ul>
+
+<body class="bg">
+<?php
+date_default_timezone_set("PRC");
+?>
