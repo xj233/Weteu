71b0d20b8790b14ea8a4daedbf9dcc8eedc1a55c
xj修行者 <xj@ioig.net>
2019-02-12 10:23:58 +0800

    add background

--- common/footer.php
+++ common/footer.php
@@ -1,5 +1,17 @@
 <footer>
-<div class="copyright">&copy; <a rel="nofollow" href="https://www.ioig.net/archives/sq.html">矢量实验室</a>丨风栖社区</div>
-</footer>
- </body>
-  </html>
+  <div class="copyright">&copy;
+    <a rel="nofollow" href="https://www.ioig.net/archives/sq.html">矢量实验室</a>丨风栖社区</div></footer>
+<script src="https://cdn.bootcss.com/bubbly-bg/1.0.0/bubbly-bg.js"></script>
+<script>bubbly({
+    colorStart: '#fff4e6',
+    colorStop: '#ffe9e4',
+    blur: 1,
+    compose: 'source-over',
+    bubbleFunc: () = >`hsla($ {
+      Math.random() * 50
+    },
+    100 % , 50 % , .3)`
+  });</script>
+</body>
+
+</html>
